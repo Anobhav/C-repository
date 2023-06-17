daeaa5078eefa18533cf0d13ef9f93cd4685a18c
Anobhav Singh <63008107+Anobhav@users.noreply.github.com>
2023-06-17 16:28:27 +0530

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Example
+{
+        int a,b;
+    public:
+        Example()
+        {
+
+        }
+        Example(int,int);
+        void display();
+};
+Example::Example(int x,int y)
+{
+    a = x;
+    b = y;
+}
+void Example::display()
+{
+    cout<<a<<" "<<b<<endl;
+}
+int main()
+{
+    Example E1(10,20);
+    E1.display();
+    Example E2;
+    E2 = E1;
+    E2.display();
+    return(0);
+}
